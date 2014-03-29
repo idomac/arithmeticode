8ceed5379673ea888c52f45537da33937ff31e68
quanet <quanet@163.com>
2014-03-29 18:12:40 +0800

    增加约瑟夫环展示

--- training/src/main/java/org/idomac/training/yue/YueDemo.java
+++ training/src/main/java/org/idomac/training/yue/YueDemo.java
@@ -1,5 +1,7 @@
 package org.idomac.training.yue;
 
+import java.util.Scanner;
+
 /**
  * @author : lihaoquan
  *
@@ -8,8 +10,51 @@ package org.idomac.training.yue;
  */
 public class YueDemo {
 
+    private static class Node{
+        public int no;
+        public Node next;
+        public Node(int no) {
+            this.no = no;
+        }
+    }
+
+    /**
+     * 约瑟夫函数处理
+     */
+    public static void yuesefu(int totalNum, int countNum) {
+
+        /**
+         * 初始化一个闭环
+         */
+        Node header = new Node(1);
+        Node currentPointer = header;
+        for(int i = 2; i<= totalNum;i++) {
+            currentPointer.next = new Node(i);
+            currentPointer = currentPointer.next;
+        }
+        currentPointer.next = header; //这一步形成环状
+
+        System.out.println("以下是出列的顺序节点编号 : ");
+        while(currentPointer!=currentPointer.next) {//环还存在的情况:
+            for(int i = 1; i< countNum;i++) {
+                currentPointer = currentPointer.next;
+            }
+            //枪毙不幸的节点
+            System.out.println(currentPointer.next.no);
+            currentPointer.next = currentPointer.next.next;
+        }
+        System.out.println("----------"+currentPointer.next.no);
+    }
+
+
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入总人数:");
+        int totalNum = scanner.nextInt();
+        System.out.println("请输入报数的大小:");
+        int cycleNum = scanner.nextInt();
 
+        yuesefu(totalNum,cycleNum);
     }
 }
