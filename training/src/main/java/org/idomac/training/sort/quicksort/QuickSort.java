27bfbc07fb85d3e39ebe5449a73d0c48b00531c1
quanet <quanet@163.com>
2014-03-31 22:41:30 +0800

    增加快速排序的相关函数

--- training/src/main/java/org/idomac/training/sort/quicksort/QuickSort.java
+++ training/src/main/java/org/idomac/training/sort/quicksort/QuickSort.java
@@ -17,11 +17,49 @@ public class QuickSort {
         sort(a,0,a.length-1);//快速排序
     }
 
+
+    /**
+     * 快速排序算法
+     * @param a
+     * @param left
+     * @param right
+     */
     public static void sort(int[] a,int left,int right) {
 
+        if(right <= left) return;
+        int j = partition(a,left,right); //进行切分
+        sort(a,left,j-1);//左半部快速排序
+        sort(a,j+1,right);//右半部分快速排序
     }
 
 
+    /**
+     * 切分函数
+     * @param a
+     * @param left
+     * @param right
+     * @return
+     */
+    public static int partition(int[] a,int left,int right) {
+        int i = left;
+        int j = right+1;
+        int v = a[i];
+        while(true) {
+            while(a[++i] < v) if(i == right) break;
+            while(v<a[--j]) if(j==left) break;
+            if(i>=j) break;
+            exchange(a,i,j);
+        }
+        exchange(a,left,j);
+        return j;
+    }
+
+
+    public static void exchange(int[] a,int i,int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
 
     public static void main(String[] args) {
 
@@ -41,5 +79,9 @@ public class QuickSort {
         }
 
         sort(datas);
+
+        for(int i = 0; i<datas.length;i++) {
+            System.out.println("# "+datas[i]);
+        }
     }
 }
