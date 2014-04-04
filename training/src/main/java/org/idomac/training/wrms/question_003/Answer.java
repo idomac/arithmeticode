d7625f2dbfd1a074282b1a1a86782971840f7fdf
lihaoquan <quanet@163.com>
2014-04-04 11:24:43 +0800

    创建第三个问题

---
+++ training/src/main/java/org/idomac/training/wrms/question_003/Answer.java
@@ -0,0 +1,8 @@
+package org.idomac.training.wrms.question_003;
+
+/**
+ * Author : lihaoquan
+ * Description :
+ */
+public class Answer {
+}
---
+++ training/src/test/java/org/idomac/training/wrms/question_003/AnswerTest.java
@@ -0,0 +1,14 @@
+package org.idomac.training.wrms.question_003;
+
+/**
+ * Author : lihaoquan
+ * Description :
+ * 输入一个整形数组，数组里有正数也有负数。
+ * 数组中连续的一个或多个整数组成一个子数组，每个子数组都有一个和。
+ *  求所有子数组的和的最大值。要求时间复杂度为 O(n)。
+ *
+ *  例如输入的数组为 1, -2, 3, 10, -4, 7, 2, -5，和最大的子数组为 3, 10, -4, 7, 2，
+ *  因此输出为该子数组的和 18
+ */
+public class AnswerTest {
+}
