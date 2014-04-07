46f30a21cbea7db23273856b9b132656ec7bd2c8
quanet <quanet@163.com>
2014-04-07 12:05:24 +0800

    提交第五题目

---
+++ training/src/main/java/org/idomac/training/wrms/question_005/CustomStack.java
@@ -0,0 +1,15 @@
+package org.idomac.training.wrms.question_005;
+
+/**
+ * @author : lihaoquan
+ * 输入 n 个整数,输出其中最小的 k 个
+ * 例如输入 1,2,3,4,5,6,7 和 8 这 8 个数字,则最小的 4 个数字为 1,2,3 和 4
+ *
+ * ***********************************************
+ * 参考资料:
+ *
+ */
+public class CustomStack {
+
+
+}
---
+++ training/src/test/java/org/idomac/training/wrms/question_005/CustomStackTest.java
@@ -0,0 +1,14 @@
+package org.idomac.training.wrms.question_005;
+
+import org.junit.Test;
+
+/**
+ * @author : lihaoquan
+ */
+public class CustomStackTest {
+
+    @Test
+    public void getMin() {
+
+    }
+}
