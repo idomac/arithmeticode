f67847d5396ec72a562802d5ed893915cc63674a
lihaoquan <quanet@163.com>
2014-04-08 16:03:41 +0800

    创建第三个问题

--- training/src/main/java/org/idomac/training/wrms/question_006/BottomArrGenerator.java
+++ training/src/main/java/org/idomac/training/wrms/question_006/BottomArrGenerator.java
@@ -13,6 +13,51 @@ package org.idomac.training.wrms.question_006;
  * 数值: 0,1,2,3,4,5,6,7,8,9
  * 分配: 6,2,1,0,0,0,1,0,0,0
  * 0 在下排出现了 6 次，1 在下排出现了 2 次,2 在下排出现了 1 次，3 在下排出现了 0 次...
+ *
+ * *******************************************************8888
+ * 参考:
+ * 1.http://zhuiyi74.iteye.com/blog/839742
+ *
  */
 public class BottomArrGenerator {
+
+    /**
+     * 结构结果生成
+     * @param n
+     * @return
+     */
+    public static int[] generate(int n) {
+
+        if(n<4) {
+            System.out.println("请输入不小于4的整数");
+            return null;
+        }
+
+        int[] top = new int[n];
+        int[] buttom = new int[n];
+
+        //把相关的数字设置进去
+        for (int i = 0 ; i < top.length; i++) {
+            top[i] = i;
+
+            System.out.println(top[i]);
+        }
+        return null;
+    }
+
+    /**
+     * .统计数字n在数组arr中出现的次数
+     * @param num
+     * @param arr
+     * @return
+     */
+    public static int getCount(int num,int [] arr) {
+        int count = 0;
+        for(int i : arr) {
+            if(num == i) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
--- training/src/test/java/org/idomac/training/wrms/question_006/BottomArrGeneratorTest.java
+++ training/src/test/java/org/idomac/training/wrms/question_006/BottomArrGeneratorTest.java
@@ -1,8 +1,16 @@
 package org.idomac.training.wrms.question_006;
 
+import org.junit.Test;
+
 /**
  * Author : lihaoquan
  * Description :
  */
 public class BottomArrGeneratorTest {
+
+    @Test
+    public void generate() {
+
+        BottomArrGenerator.generate(10);
+    }
 }
