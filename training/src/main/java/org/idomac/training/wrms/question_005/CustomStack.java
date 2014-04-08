39244452842f6f11925fcfbbd21ebd78713a3961
lihaoquan <quanet@163.com>
2014-04-08 11:21:31 +0800

    提交第5个问题

--- training/src/main/java/org/idomac/training/wrms/question_005/CustomStack.java
+++ training/src/main/java/org/idomac/training/wrms/question_005/CustomStack.java
@@ -7,9 +7,56 @@ package org.idomac.training.wrms.question_005;
  *
  * ***********************************************
  * 参考资料:
+ * 1. http://blog.csdn.net/fg2006/article/details/6746416
+ * 2. http://events.36kr.com/oday-201404-gz/#signed
  *
  */
 public class CustomStack {
 
+    /**
+     * 获取数组a的k个最小的数组
+     * @param a
+     * @param k
+     * @return
+     */
+    public static int[] getMins(int[] a,int k) {
+        int[] target = new int[k];
+        for(int i = 0; i< k;i++) {
+            target[i] = a[i];
+        }
+        buildHeap(target);
+        for(int j = k;j<a.length;j++){
+            if(a[j]<target[0]){
+                target[0] = a[j];
+                maxHeap(target,1,k);
+            }
+        }
+        return target;
+    }
 
+    /**
+     * 建最大堆
+     * @param arr
+     */
+    public static void buildHeap(int[] arr) {
+        int heapsize = arr.length;
+        for(int i=arr.length/2;i>0;i--)
+            maxHeap(arr,i,heapsize);
+    }
+
+    public static void maxHeap(int arr[],int i,int heapsize) {
+        int largest = i;
+        int left = 2*i;
+        int right = 2*i+1;
+        if(left<=heapsize&&arr[i-1]<arr[left-1])
+            largest = left;
+        if(right<=heapsize&&arr[largest-1]<arr[right-1])
+            largest = right;
+        if(largest!=i){
+            int temp = arr[i-1];
+            arr[i-1] = arr[largest-1];
+            arr[largest-1] = temp;
+            maxHeap(arr,largest,heapsize);
+        }
+    }
 }
--- training/src/test/java/org/idomac/training/wrms/question_005/CustomStackTest.java
+++ training/src/test/java/org/idomac/training/wrms/question_005/CustomStackTest.java
@@ -9,6 +9,10 @@ public class CustomStackTest {
 
     @Test
     public void getMin() {
+        int a[] = {1,3,4,2,7,8,9,10,14,16};
+        int[] krr = CustomStack.getMins(a,4);
 
+        for(int i =0;i<krr.length;i++)
+            System.out.println(krr[i]);
     }
 }
