43e942886e75bf73a3e4862030b9196a2aa75713
quanet <quanet@163.com>
2014-04-11 00:15:55 +0800

    增加第七题题目

--- training/src/main/java/org/idomac/training/wrms/question_007/Node.java
+++ training/src/main/java/org/idomac/training/wrms/question_007/Node.java
@@ -2,6 +2,11 @@ package org.idomac.training.wrms.question_007;
 
 /**
  * @author : lihaoquan
+ *
+ * 出俩个单向链表的头指针,比如 h1,h2,判断这俩个链表是否相交。 为了简化问题,我们假设俩个链表均不带环。
+ * 问题扩展:
+ * 1.如果链表可能有环列?
+ * 2.如果需要求出俩个链表相交的第一个节点列?
  */
 public class Node {
 }
