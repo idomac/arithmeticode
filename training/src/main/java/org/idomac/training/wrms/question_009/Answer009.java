0ed5fc47b0a96a51ec43c8f1df8cd6e37ee9e3b9
quanet <quanet@163.com>
2014-04-13 22:38:04 +0800

    增加第九道题目

---
+++ training/src/main/java/org/idomac/training/wrms/question_009/Answer009.java
@@ -0,0 +1,9 @@
+package org.idomac.training.wrms.question_009;
+
+/**
+ * @author : lihaoquan
+ * 判断整数序列是不是二元查找树的后序遍历结果
+ * 题目:输入一个整数数组,判断该数组是不是某二元查找树的后序遍历的结果。 如果是返回 true,否则返回 false。
+ */
+public class Answer009 {
+}
---
+++ training/src/test/java/org/idomac/training/wrms/question_009/Answer009Test.java
@@ -0,0 +1,7 @@
+package org.idomac.training.wrms.question_009;
+
+/**
+ * @author : lihaoquan
+ */
+public class Answer009Test {
+}
