e95a182726567970db56095f15e54fa8e815542d
lihaoquan <quanet@163.com>
2014-04-14 11:21:06 +0800

    提交第九道题目

--- training/src/test/java/org/idomac/training/wrms/question_009/Answer009Test.java
+++ training/src/test/java/org/idomac/training/wrms/question_009/Answer009Test.java
@@ -1,7 +1,19 @@
 package org.idomac.training.wrms.question_009;
 
+import org.junit.Test;
+
 /**
  * @author : lihaoquan
  */
 public class Answer009Test {
+
+    @Test
+    public void verify() {
+        int[] arr = {5, 7, 6, 9, 11, 10, 8}; //Yes
+
+        //int[] arr = {7, 4, 6, 5}; // Not
+
+        boolean flag =  Answer009.verifySquenceOfBST(arr,7);
+        System.out.println("是否是后序遍历:"+flag);
+    }
 }
