977311dafbad270a6c57de90bfc15b6346c09d74
lihaoquan <quanet@163.com>
2014-04-14 11:28:08 +0800

    创建第十个问题

---
+++ training/src/main/java/org/idomac/training/wrms/question_010/WordOrder.java
@@ -0,0 +1,13 @@
+package org.idomac.training.wrms.question_010;
+
+/**
+ * Author : lihaoquan
+ * Description : 翻转句子中单词的顺序。
+ *
+ * 输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。
+ * 句子中单词以空格符隔开。为简单起见，标点符号和普通字母一样处理。
+ * 例如输入“I am a student.”，则输出“student. a am I”。
+ */
+public class WordOrder {
+
+}
