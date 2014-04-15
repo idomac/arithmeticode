397cbc2aefb0f29840d832d1be6864278d6a7001
lihaoquan <quanet@163.com>
2014-04-15 16:53:17 +0800

    创建第十一个问题

---
+++ training/src/main/java/org/idomac/training/wrms/question_011/Answer011.java
@@ -0,0 +1,13 @@
+package org.idomac.training.wrms.question_011;
+
+/**
+ * Author : lihaoquan
+ * Description :
+ * 求二叉树中节点的最大距离...
+ * 如果我们把二叉树看成一个图，父子节点之间的连线看成是双向的，
+ * 我们姑且定义"距离"为两节点之间边的个数。
+ * 写一个程序，
+ * 求一棵二叉树中相距最远的两个节点之间的距离。
+ */
+public class Answer011 {
+}
